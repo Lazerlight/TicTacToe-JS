e70d9cfc5dc622cce074881662c63a2cf1b14400
Lazerlight <snndsnns2@gmail.com>
2023-01-29 01:53:11 +0200

    Update-04

--- script.js
+++ script.js
@@ -1,8 +1,38 @@
+const CIRCLE_CLASS = "circle";
+const X_CLASS = "x";
 const cellEl = document.querySelectorAll("[data-cell]");
+const boardEl = document.getElementById("board");
+let circleTurn;
 
-cellEl.forEach((cell) => {
-  cell.addEventListener("click", handleClick, { once: true });
-});
-function handleClick() {
-  console.log("KOROROR");
+startGame();
+
+function startGame() {
+  circleTurn = false;
+  cellEl.forEach((cell) => {
+    cell.addEventListener("click", handleClick, { once: true });
+  });
+  setBoardHover();
+}
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS;
+  placeMark(cell, currentClass);
+  swapTurn();
+  setBoardHover();
+}
+
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+function swapTurn() {
+  circleTurn = !circleTurn;
+}
+function setBoardHover() {
+  boardEl.classList.remove(CIRCLE_CLASS);
+  boardEl.classList.remove(X_CLASS);
+  if (circleTurn) {
+    boardEl.classList.add(CIRCLE_CLASS);
+  } else {
+    boardEl.classList.add(X_CLASS);
+  }
 }
